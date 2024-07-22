2945b46aaab8613404174365a74c7140c7125615
SaravananSundar <saravananmech020797@gmail.com>
2024-07-22 13:29:44 +0530

    second commit

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-QQTE6B34.mjs";import"./chunk-Y3H3VZBL.mjs";import"./chunk-LOJ3LMBH.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-JTCQMZZA.mjs";import"./chunk-Y3H3VZBL.mjs";import"./chunk-LOJ3LMBH.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
